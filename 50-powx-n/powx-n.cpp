be6ca1f67b08954ccd2fb43c4f20f522ea76a067
padminijatav <pdmnjatav@gmail.com>
2026-02-02 23:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.45%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==1) return 1.0;
+        if(x==0) return 0.0;
+        if(n==1) return x;
+        if(n==0) return 1.0;
+        if(x==-1 && n%2==0) return 1.0;
+        if(x==-1 && n%2!=0) return -1.0;
+        long binform = n;
+        if(n<0){
+            x=1/x;
+            binform=-binform;
+        }
+        double ans = 1;
+        while(binform>0){
+            if(binform%2==1){
+                ans=ans*x;
+            }
+            x=x*x;
+            binform=binform/2;
+        }
+        return ans;
+    }
+};
