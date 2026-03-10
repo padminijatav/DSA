c04cbe225224d06cf493f2cb6addf927f1533be5
padminijatav <pdmnjatav@gmail.com>
2026-03-11 00:21:51 +0530

    Time: 19 ms (94.35%) | Memory: 70.9 MB (95.81%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=1;
+        while(i<nums.size()){
+            if(nums[i]==nums[i-1]) return true;
+            i++;
+        }
+        return false;
+    }
+};
