d97a4ca95f0298c24710caee69dcdb711f56864a
padminijatav <pdmnjatav@gmail.com>
2026-03-23 23:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (14.54%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -3,7 +3,7 @@ public:
     string toLowerCase(string s) {
         string res;
         for(int i=0;i<s.size();i++){
-            res+=tolower(s[i]);
+            res+= tolower(s[i]);
         }
         return res;
     }
