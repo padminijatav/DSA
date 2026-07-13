b7b366b9c656d717fe1e5a21c47714168940b654
padminijatav <pdmnjatav@gmail.com>
2026-07-13 23:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (56.54%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<words.size();i++){
             acro+=words[i][0];
         }
-        if(s==acro) return true;
-        return false;
+        
+        return s==acro;
     }
 };
