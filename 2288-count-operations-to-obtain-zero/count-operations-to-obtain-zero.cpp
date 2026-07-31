39b59f279c9a9da14d1568fcf2b3a4c08824a2fa
padminijatav <pdmnjatav@gmail.com>
2026-07-31 22:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.14%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -5,12 +5,11 @@ public:
         while(num1!=0 && num2!=0){
             if(num1>=num2) {
                 num1-=num2;
-                op++;
             }
             else{
                 num2-=num1;
-                op++;
             }
+            op++;
         }
         return op;
     }
