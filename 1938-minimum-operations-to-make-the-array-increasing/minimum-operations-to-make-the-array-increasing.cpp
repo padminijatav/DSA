b99df77f0acb12e55fe29e78c595a30401b07410
padminijatav <pdmnjatav@gmail.com>
2026-08-23 22:45:15 +0530

    Time: 7 ms (84.78%) | Memory: 19.6 MB (29.62%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -4,10 +4,9 @@ public:
         if(nums.size()==0 || nums.size()==1) return 0;
         int op=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]<=nums[i-1]){
-                op+=nums[i-1]-nums[i]+1;
-                nums[i]=nums[i-1]+1;
-            }
+            if(nums[i]>nums[i-1]) continue;
+            op+=nums[i-1]-nums[i]+1;
+            nums[i]=nums[i-1]+1;
         }
         return op;
     }
